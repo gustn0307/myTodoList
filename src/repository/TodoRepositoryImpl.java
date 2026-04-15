f50d0c71e81eb061d460795060ec4a9050e672b7
gustn0307 <gustn0307@hanmail.net>
2026-04-15 11:24:42 +0900

    #3 날짜로 할 일 리스트 반환하는 메서드 구현 완료

--- src/repository/TodoRepositoryImpl.java
+++ src/repository/TodoRepositoryImpl.java
@@ -28,7 +28,18 @@ public class TodoRepositoryImpl implements TodoRepository{
 
     @Override
     public List<Todo> findByDate(String date) {
-        return List.of();
+        List<Todo> todos = new ArrayList<>();
+        for (String key : todoMap.keySet()) { // 순회해서 해당 날짜의 할 일 List 반환
+            if(date.equals(key)){ // 일치하는 날짜 있으면 리스트에 추가
+                return todoMap.get(key); // 일치하는 날짜의 할 일 리스트 반환
+            }
+        }
+
+        // 순회해서 입력한 날짜가 없으면 날짜와 빈 리스트 추가
+        // 컬렉션은 null 대신 빈 컬렉션을 반환한다.(실무 규칙)
+        // 컬렉션은 Optional 사용하지 않는다. (단일 객체 조회는 Optional 사용함)
+        todoMap.put(date, new ArrayList<>()); // 날짜를 추가하고 null 방지를 위해 빈 리스트를 추가(실무 규칙)
+        return todoMap.get(date); // 찾는 날짜가 없으면 빈 리스트를 반환
     }
 
     @Override
